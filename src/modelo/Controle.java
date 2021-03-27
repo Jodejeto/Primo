023836b11cb514fceaeb7c019136f25ec97759ef
Renan Ariel <renanariel50@gmail.com>
2021-03-26 22:02:39 -0300

    continuação

--- src/modelo/Controle.java
+++ src/modelo/Controle.java
@@ -6,10 +6,21 @@ public class Controle extends absPropriedades
     public Controle(String numero)
       {
         super(numero);
+        this.executar();
       }
     
     private void executar()
       {
-        
+        this.setMensagem("");
+        Validacao validacao = new Validacao(this.getNumero());
+        if (validacao.getMensagem().equals(""))
+        {
+            Primo primo = new Primo(validacao.getNum());
+            this.setMensagem(primo.getMensagem());
+        }
+        else
+        {
+            this.setMensagem(validacao.getMensagem());
+        }
       }
 }
--- src/modelo/Primo.java
+++ src/modelo/Primo.java
@@ -6,10 +6,19 @@ public class Primo extends absPropriedades
     public Primo(int num)
       {
         super(num);
+        this.verificarPrimo();
       }
     
     private void verificarPrimo()
       {
-        
+        this.setMensagem("É primo");
+        for (int i = 2; i < this.getNum() / 2 + 1; i++)
+        {
+            if (this.getNum() % i == 0)
+            {
+                this.setMensagem("Não é primo");
+                break;
+            }
+        }
       }
 }
